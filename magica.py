3d82ec78a57c19d84a4b548324c396626d5bf98a
Matheus Coutinho <matheus.magno7@gmail.com>
2025-10-17 21:45:06 -0300

    jogo de mágica

---
+++ magica.py
@@ -0,0 +1,18 @@
+import os
+
+os.system('cls')
+
+print('Olhe as cartas abaixo e escolha uma: ')
+print('------')
+print('Q♣ K♦ J♥ Q♥ J♣ K♠')
+input('Pressione Enter quando escolher a carta...')
+
+os.system('cls')
+print('A Carta que você pensou...')
+input('Pressione Enter para revelar a carta...')
+
+print()
+print('Desapareceu')
+print()
+print('J♠ Q♦ J♦ Q♠ K♥')
+input()
