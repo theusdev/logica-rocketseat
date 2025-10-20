5445d650b5fea258450cf43fb816364c4a07d6e5
Matheus Coutinho <matheus.magno7@gmail.com>
2025-10-19 22:27:19 -0300

    up teste_condicional

---
+++ teste_condicional.py
@@ -0,0 +1,12 @@
+print('Seja bem vindo ao DETRAN')
+print('Este é o nosso novo teste para tirar a CNH')
+print()
+
+idade = int(input('Digite aqui a sua idade: '))
+
+if (idade >= 18):
+  print('Pode tirar a carteira')
+else:
+  print('Proibido tirar a carteira, você ainda não é maior de idade.')
+
+print('Fim do Teste.')
