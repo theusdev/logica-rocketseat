2de2098c5bc600f7c4738cec36a8f34d461dfc12
Matheus Coutinho <matheus.magno7@gmail.com>
2025-10-19 22:37:17 -0300

    up teste_notas

---
+++ teste_notas.py
@@ -0,0 +1,8 @@
+nota = int(input('Digite a sua nota: '))
+
+if (nota >= 7):
+  print('Parabéns você Passou de ano !')
+elif (nota < 5):
+  print('Infelizmente você não passou de ano. ')
+else:
+  print('Você está na recuperação ! :/ ')
